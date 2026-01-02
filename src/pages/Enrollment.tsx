6c9ae0ef798fa50faa008ef0eef4223a6e95055a
WesleyIreland109 <wesley.ireland109@live.com>
2026-01-02 10:22:43 -0600

    Updated the enrollments page

--- src/pages/Enrollment.tsx
+++ src/pages/Enrollment.tsx
@@ -1,3 +1,36 @@
 import React from 'react';
-const Enrollment = () => <div><h2>Enrollment</h2><p>Enroll your child at Little Hearts.</p></div>;
+import styles from './Enrollment.module.css';
+
+const Enrollment: React.FC = () => {
+  const formUrl = 'https://docs.google.com/forms/d/e/1FAIpQLSer9HtoF_XK_QkNuT90PVXZHJOCPxxq0x5IQ8kJG3edTflpPQ/viewform';
+
+  const handleClick = () => {
+    window.open(formUrl, '_blank', 'noopener,noreferrer');
+  };
+
+  return (
+    <div className={styles.enrollmentPage}>
+      <h1 className={styles.preEnrollmentTitle}>PRE-ENROLLMENT</h1>
+
+      <p className={styles.subTitle}>CLICK HERE TO PRE- ENROLL</p>
+
+      <div className={styles.buttonRow}>
+        <button className={styles.preEnrollmentButton} onClick={handleClick}>
+          PRE-ENROLLMENT INTEREST FORM!
+        </button>
+      </div>
+
+      <p className={styles.offerText}>
+        WE OFFER FULL TIME, PART TIME, AND DROP IN CHILDCARE!
+      </p>
+
+      <h2 className={styles.targetOpeningTitle}>
+        TARGET OPENING
+        <br />
+        FALL 2026
+      </h2>
+    </div>
+  );
+};
+
 export default Enrollment;
